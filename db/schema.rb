85ea5e4399d0d0990c16d9ee4a09bec812aaa373
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2016-06-21 23:55:54 +0500

    Ad: Added model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621185303) do
+ActiveRecord::Schema.define(version: 20160621185521) do
+
+  create_table "ads", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "url",        limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "channels", force: :cascade do |t|
     t.string   "name",        limit: 255
