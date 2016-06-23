6131ab9b8c1b981e508a5c7efbc6580a651d3efc
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2016-06-23 23:45:28 +0500

    Merged dev2 into dev

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'static_pages#home'
+  get 'news', to: 'static_pages#news', as: :news
+  get 'business', to: 'static_pages#business', as: :business
+  get 'sports', to: 'static_pages#sports', as: :sports
+  get 'general', to: 'static_pages#general', as: :general
+
 end
