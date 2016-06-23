ff358c6ae37d21e23e0b04fcf29b14870673d33f
azaz <azaz.nuces@gmail.com>
2016-06-23 23:39:27 +0500

    Initial styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'news', to: 'static_pages#news', as: :news
+  get 'business', to: 'static_pages#business', as: :business
+  get 'sports', to: 'static_pages#sports', as: :sports
+  get 'general', to: 'static_pages#general', as: :general
+
 end
